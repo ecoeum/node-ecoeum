0f5709b4816bbe7972bb46f4fadaa7d9b3de85a8
little51 <little51@126.com>
2018-03-29 17:32:58 +0800

    modify api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 'use strict';
 const cors = require('koa2-cors');
 class Api {
-    constructor(node, blockchain, account, miner,db,peer) {
+    constructor(node, blockchain, account, miner, db, peer) {
         const Koa = require('koa');
         global.app = new Koa();
 
-        // 具体参数我们在后面进行解释
         app.use(cors({
             origin: function (ctx) {
-                return "*"; // 允许来自所有域名请求
+                return "*";
             },
             exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
             maxAge: 5,
@@ -16,16 +15,15 @@ class Api {
             allowMethods: ['GET', 'POST', 'DELETE'],
             allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
         }));
-        require('./routers')(blockchain,account,peer);
+        require('./routers')(blockchain, account, peer);
         global.blockchain = blockchain;
 
     }
 
     listen(host, port) {
         app.listen(port);
-        console.log(new Date().toLocaleString() + " compile-chain api  server listen on " + port)
+        console.log(new Date().toLocaleString() + " node-ecoeum api  server listen on " + port)
     }
 }
 
 module.exports = Api;
-
